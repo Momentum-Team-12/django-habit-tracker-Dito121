ed27eb79cf5fc3936f192ed77306e64fca7150a9
Gerardo Ayes <gerardoayes92@gmail.com>
2022-05-18 11:57:50 -0400

    server runs, nothing shows up though

--- habit/admin.py
+++ habit/admin.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+from .models import CustomUser, Habit, DateRecord
 
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-    list_display = ["email", "username",]
 
-admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(CustomUser)
+admin.site.register(Habit)
+admin.site.register(DateRecord)
--- habit/models.py
+++ habit/models.py
@@ -2,9 +2,16 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class Meta:
+    db_table = 'date_record'
+    constraints = [
+        models.UniqueConstraint(fields=['habit', 'date'], name='unique_date_record')
+    ]
+
+
 class CustomUser(AbstractUser):
-    username = models.CharField(max_length=255, unique=True)
-    email = models.EmailField(max_length=255, unique=True)
+    def __repr__(self):
+        return f"<User username={self.username}>"
 
     def __str__(self):
         return self.username
@@ -12,14 +19,19 @@ class CustomUser(AbstractUser):
 
 class Habit(models.Model):
     name = models.CharField(max_length=255)
-    target = models.CharField(max_length=255)
+    target = models.IntegerField()
     custom_user = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='habits', max_length=255)
     unit = models.CharField(max_length=255)
-    frequency = models.CharField(max_length=255)
+    frequency = models.CharField(max_length=255, null=True, blank=True)
     created_at = models.DateField(auto_now_add=True)
-    ends_on = models.DateField()
+    starts_on = models.DateField(null=True, blank=True)
+    ends_on = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
-class DATERECORD(models.Model):
+class DateRecord(models.Model):
     habit = models.ForeignKey('Habit', on_delete=models.CASCADE, related_name='daterecords', max_length=255)
     actual = models.IntegerField()
+    date = models.DateField(null=True, blank=True)
