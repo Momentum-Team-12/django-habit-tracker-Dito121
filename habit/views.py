ac1f46d5b204a2f872b8f3afed8e697d55d55854
Gerardo Ayes <gerardoayes92@gmail.com>
2022-05-19 12:23:09 -0400

    in the middle of creating add, edit, delete for DateRecords

--- habit/views.py
+++ habit/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-from .forms import HabitForm
+from .forms import HabitForm, DateRecordForm
 from .models import User, Habit, DateRecord
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 
 
 @login_required
-def list_habits(request):
+def log_out(request):
     logout(request)
     return redirect("log_out")
 
@@ -22,8 +22,8 @@ def list_habits(request):
 @login_required
 def habit_details(request, pk):
     habit = Habit.objects.get(pk=pk)
-    # date_records = DateRecord.objects.filter(habit=request.habit)
-    return render(request, "habit/habit_details.html", {'habit': habit})
+    date_records = DateRecord.objects.filter(habit=habit)
+    return render(request, "habit/habit_details.html", {'habit': habit, 'date_records': date_records})
 
 
 @login_required
@@ -62,6 +62,52 @@ def edit_habit(request, pk):
 
 @login_required
 def delete_habit(request, pk):
+    habit = get_object_or_404(Habit, pk=pk)
+    if request.method == 'POST':
+        habit.delete()
+        return redirect(to='list_habits')
+
+    return render(request, "habit/delete_habit.html", {"habit": habit})
+
+
+@login_required
+def add_date_record(request, pk):
+    habit = get_object_or_404(Habit, pk=pk)
+    if request.method == 'GET':
+        form = DateRecordForm()
+    else:
+        form = DateRecordForm(data=request.POST)
+        if form.is_valid():
+            date_record = form.save(commit=False)
+            date_record.user = request.user
+            date_record.habit = habit
+            date_record.save()
+            return redirect(to='list_date_record')
+
+    return render(request, "habit/add_date_record.html", {"form": form})
+
+
+@login_required
+def edit_date_record(request, pk):
+    habit = get_object_or_404(Habit, pk=pk)
+    if request.method == 'GET':
+        form = HabitForm(instance=habit)
+    else:
+        form = HabitForm(data=request.POST, instance=habit)
+        if form.is_valid():
+            habit = form.save(commit=False)
+            habit.user = request.user
+            habit.save()
+            return redirect(to='list_habits')
+
+    return render(request, "habit/edit_habit.html", {
+        "form": form,
+        "habit": habit,
+    })
+
+
+@login_required
+def delete_date_record(request, pk):
     habit = get_object_or_404(Habit, pk=pk)
     if request.method == 'POST':
         habit.delete()
