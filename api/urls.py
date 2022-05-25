1771c9c95a7e062e891373c856fdaf9429c12ec1
Gerardo Ayes <gerardoayes92@gmail.com>
2022-05-25 16:37:21 -0400

    habit detail api done, now need create and edit and delete ability

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from api import views as api_views
 
 urlpatterns = [
-    path('habits', api_views.HabitListView.as_view(), name='habit-list-api')
+    path('habits', api_views.HabitListView.as_view(), name='habit-list-api'),
+    path('habit/<int:pk>/', api_views.HabitDetailView.as_view(), name='habit-detail-api'),
 ]
--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import authentication, permissions
 from habit.models import Habit
 from .serializers import HabitSerializer
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
 
 
 class HabitListView(APIView):
@@ -15,3 +16,12 @@ class HabitListView(APIView):
         habits = Habit.objects.filter(user=request.user)
         serializer = HabitSerializer(habits, many=True)
         return Response(serializer.data)
+
+
+class HabitDetailView(RetrieveAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+
+class HabitCreateView(CreateAPIView):
+    pass
