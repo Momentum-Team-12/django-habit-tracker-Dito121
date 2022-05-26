f3204485b27be953d939f47b748c3a8fa2174cbe
Gerardo Ayes <gerardoayes92@gmail.com>
2022-05-26 02:40:34 -0400

    modified views and urls within my api to be viewsets and routers

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,24 @@
-from django.urls import path
-from api import views as api_views
+from .views import HabitViewSet, UserViewSet
+from rest_framework.routers import DefaultRouter
 
 
-urlpatterns = [
-    path('', api_views.api_root),
-    path('users', api_views.UserListView.as_view(), name='user-list-api'),
-    path('habits', api_views.HabitListView.as_view(), name='habit-list-api'),
+router = DefaultRouter()
+router.register(r'habits', HabitViewSet, 'habits')
+router.register(r'users', UserViewSet, 'users')
+urlpatterns = router.urls
 
-    path('habit/create/', api_views.HabitCreateView.as_view(), name='habit-create-api'),
-    path('habit/<int:pk>/', api_views.HabitDetailView.as_view(), name='habit-detail-api'),
-    path('habit/<int:pk>/update/', api_views.HabitUpdateView.as_view(), name='habit-update-api'),
-    path('habit/<int:pk>/delete/', api_views.HabitDeleteView.as_view(), name='habit-delete-api'),
-]
+
+# from django.urls import path
+# from api import views as api_views
+
+
+# urlpatterns = [
+#     path('', api_views.api_root),
+#     path('users', api_views.UserListView.as_view(), name='user-list-api'),
+#     path('habits', api_views.HabitListView.as_view(), name='habit-list-api'),
+
+#     path('habit/create/', api_views.HabitCreateView.as_view(), name='habit-create-api'),
+#     path('habit/<int:pk>/', api_views.HabitDetailView.as_view(), name='habit-detail-api'),
+#     path('habit/<int:pk>/update/', api_views.HabitUpdateView.as_view(), name='habit-update-api'),
+#     path('habit/<int:pk>/delete/', api_views.HabitDeleteView.as_view(), name='habit-delete-api'),
+# ]
--- api/views.py
+++ api/views.py
@@ -1,64 +1,80 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import authentication, permissions
-from rest_framework.decorators import api_view
-from rest_framework.reverse import reverse
-
 from habit.models import Habit, User
 from .serializers import HabitSerializer, UserSerializer
-from rest_framework.generics import (
-    CreateAPIView,
-    RetrieveAPIView,
-    UpdateAPIView,
-    DestroyAPIView)
+
+from rest_framework import viewsets
 
 
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list-api', request=request, format=format),
-        'habits': reverse('habit-list-api', request=request, format=format)
-    })
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
-class UserListView(APIView):
-
-    def get(self, request, format=None):
-        """
-        Return a JSON list of all users
-        """
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
-
-
-class HabitListView(APIView):
-
-    def get(self, request, format=None):
-        """
-        Return a JSON list of all habits
-        """
-        habits = Habit.objects.filter(user=request.user)
-        serializer = HabitSerializer(habits, many=True)
-        return Response(serializer.data)
-
-
-class HabitDetailView(RetrieveAPIView):
+class HabitViewSet(viewsets.ModelViewSet):
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
 
 
-class HabitCreateView(CreateAPIView):
-    queryset = Habit.objects.all()
-    serializer_class = HabitSerializer
+# from django.shortcuts import render
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import authentication, permissions
+# from rest_framework.decorators import api_view
+# from rest_framework.reverse import reverse
+
+# from habit.models import Habit, User
+# from .serializers import HabitSerializer, UserSerializer
+# from rest_framework.generics import (
+#     CreateAPIView,
+#     RetrieveAPIView,
+#     UpdateAPIView,
+#     DestroyAPIView)
 
 
-class HabitUpdateView(UpdateAPIView):
-    queryset = Habit.objects.all()
-    serializer_class = HabitSerializer
+# @api_view(['GET'])
+# def api_root(request, format=None):
+#     return Response({
+#         'users': reverse('user-list-api', request=request, format=format),
+#         'habits': reverse('habit-list-api', request=request, format=format)
+#     })
 
 
-class HabitDeleteView(DestroyAPIView):
-    queryset = Habit.objects.all()
-    serializer_class = HabitSerializer
+# class UserListView(APIView):
+
+#     def get(self, request, format=None):
+#         """
+#         Return a JSON list of all users
+#         """
+#         users = User.objects.all()
+#         serializer = UserSerializer(users, many=True)
+#         return Response(serializer.data)
+
+
+# class HabitListView(APIView):
+
+#     def get(self, request, format=None):
+#         """
+#         Return a JSON list of all habits
+#         """
+#         habits = Habit.objects.filter(user=request.user)
+#         serializer = HabitSerializer(habits, many=True)
+#         return Response(serializer.data)
+
+
+# class HabitDetailView(RetrieveAPIView):
+#     queryset = Habit.objects.all()
+#     serializer_class = HabitSerializer
+
+
+# class HabitCreateView(CreateAPIView):
+#     queryset = Habit.objects.all()
+#     serializer_class = HabitSerializer
+
+
+# class HabitUpdateView(UpdateAPIView):
+#     queryset = Habit.objects.all()
+#     serializer_class = HabitSerializer
+
+
+# class HabitDeleteView(DestroyAPIView):
+#     queryset = Habit.objects.all()
+#     serializer_class = HabitSerializer
